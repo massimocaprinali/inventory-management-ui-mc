fe93239617476495bd5b8090bfb57a8a2eab6fa8
Massimo Caprinali <massimo.caprinali@it.ibm.com>
2020-03-05 19:06:11 +0100

    retry

--- client/src/App.test.jsx
+++ client/src/App.test.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
-import * as ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom';
 import App from './App';
 import {StockItemMockService} from "./services/stock-item-mock.service";
+
 describe('App', () => {
   test('canary verifies test infrastructure', () => {
      expect(true).toEqual(true);
   });
+
   test('renders without crashing', () => {
     const div = document.createElement('div');
     ReactDOM.render(<App stockService={new StockItemMockService()}/>, div);
